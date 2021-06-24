e1bfb545df57edbf6ef5eb02dfdeec300cf4cacc
Matheus-Pessoa-Oliveira <mateusand2015@gmail.com>
2021-06-24 18:36:47 -0300

    Add project files.

---
+++ CadastroJogo/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CadastroJogo.Models;
+
+namespace CadastroJogo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var jogo = new Jogo(); // aqui criamos o projeto
+            return View(jogo); // Retornamos para a view dados
+        }
+        [HttpPost]
+        public ActionResult Index(Jogo jogo)
+        {
+            if (ModelState.IsValid) // Validamos o estado
+            {
+                return View("Resultado", jogo);
+            }
+            return View(jogo);
+        }
+        public ActionResult Resultado(Jogo jogo)
+        {
+            return View(jogo);
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ CadastroJogo/Models/Jogo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CadastroJogo.Models
+{
+    public class Jogo 
+    {
+        // GET: Jogo
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "O nome do jogo é obrigatório.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "A versão do jogo é necessária.")]
+        public int Versao { get; set; }
+
+        [Required(ErrorMessage = "Insira o desenvolvedor do jogo!")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "Informe o desenvolvedor com no minímo 3 caracteres a 200.")]
+        public string Desenvolvedor { get; set; }
+
+        [Required(ErrorMessage = "Informe o gênero do jogo!")]
+        public string Genero { get; set; }
+
+        [Range(12, 100, ErrorMessage = "Idade permitida entre 12 a 100 anos!")]
+        [Required(ErrorMessage = "Introduza a faixa etária recomendada.")]
+        public int FaixaEt { get; set; }
+
+        [Range(1958, 2021, ErrorMessage = "O ano de lançamento do jogo parte de 1958 a 2021.")]
+        [Required(ErrorMessage = "Insira o ano de lançamento do jogo.")]
+        public int AnoLanc { get; set; }
+    }
+}
